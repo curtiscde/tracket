1638084bef3ea1a3b69303be48d2c6f375ab599a
Curtis Timson <curt@live.co.uk>
2018-07-18 21:04:05 +0100

    Add callback function

---
+++ tracket-app/src/components/Callback.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class Callback extends Component {
+  render() {
+
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+}
