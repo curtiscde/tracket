11b18dc9411032e05eaeac70ad26e6e38358e774
Curtis Timson <curt@live.co.uk>
2018-07-18 20:33:05 +0100

    Add Login component

--- tracket-app/src/App.js
+++ tracket-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
-
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Login />
       </div>
     );
   }
