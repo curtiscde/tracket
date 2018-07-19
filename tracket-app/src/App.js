58898e3aa91b4454c3f3c7f7fc0a9dc839eeda91
Curtis Timson <curt@live.co.uk>
2018-07-19 15:25:07 +0100

    Add routing for callback

--- tracket-app/src/App.js
+++ tracket-app/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
+import Callback from './components/Callback';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <Login />
+        <BrowserRouter>
+        <div>
+          <Route path="/" render={(props) => <Login {...props} />} />
+          <Route path="/callback" render={(props) => <Callback {...props} />} />
+        </div>
+        </BrowserRouter>
       </div>
     );
   }
