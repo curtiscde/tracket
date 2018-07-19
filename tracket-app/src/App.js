d53112db00de7e468c8de41869e86700624b2f50
Curtis Timson <curt@live.co.uk>
2018-07-19 15:42:39 +0100

    Auth0 Integration (#2)
    * Add auth0
    
    * Add callback function
    
    * Add routing for callback
    
    * Store Auth0 JWT in localstorage
    
    * Redirect to root after setting session

--- tracket-app/src/App.js
+++ tracket-app/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
+import Callback from './components/Callback';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <Login />
+        <BrowserRouter>
+        <div>
+          <Route exact path="/" render={(props) => <Login {...props} />} />
+          <Route exact path="/callback" render={(props) => <Callback {...props} />} />
+        </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ tracket-app/src/auth/Auth.js
@@ -0,0 +1,45 @@
+import auth0 from 'auth0-js';
+import history from '../history';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: 'tracket.eu.auth0.com',
+    clientID: '16VzfYmzqI_DRgncqH2YpMqJXFcTB6hR',
+    redirectUri: 'http://localhost:3000/callback',
+    audience: 'https://tracket.eu.auth0.com/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  login() {
+    this.auth0.authorize();
+  }
+
+  handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.replace('/');
+      } else if (err) {
+        history.replace('/');
+        console.log(err);
+      }
+    });
+  }
+
+  setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the 
+    // Access Token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+};
